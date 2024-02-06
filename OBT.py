6cb7ead7ed488ef5e9c8ee683e92d124971673ef
norgads <111054919+norgads@users.noreply.github.com>
2024-02-06 18:58:36 +0300

    v 0.2
    Added score system

--- OBT.py
+++ OBT.py
@@ -11,13 +11,22 @@ data[1,1] = None
 data[2,1] = None
 data[3,1] = None
 
+data[1,2] = 1
+data[2,2] = 0
+data[3,2] = 1
+
+data[1,3] = 4
+data[2,3] = 3
+data[3,3] = 7
+
 #*Qt GUI
 
 app = QApplication([])
 app.setStyleSheet("QLabel{font-size: 38pt;}")
 win = QWidget()
 win.cur_num = 0
-win.setWindowTitle("Ocupattional Burnout Test")
+win.score = 0
+win.setWindowTitle("Occupational Burnout Test")
 win.resize(700, 500)
 btn_yes = QPushButton("Yes")
 btn_no = QPushButton("No")
@@ -34,26 +43,34 @@ vline.addLayout(hline, stretch=30)
 win.setLayout(vline)
 
 #*Events
-def next():
-    if win.cur_num == len(data)/2:
+def next_q():
+    if win.cur_num == len(data)/4: #?add automatic columns count? Maybe remove on release at all
         win.cur_num = 0
     win.cur_num += 1
     lbl_q.setText(data[win.cur_num,0])
 
+def check():
+    if data[win.cur_num, 1] == data[win.cur_num, 2]:
+        win.score += data[win.cur_num, 3]
+
+def transition():
+    print(data) #check for small size samples
+    check()
+    print(win.score)
+    next_q()
+
 def ans_yes():
     data[win.cur_num, 1] = 1
-    next()
-    print(data)
-
+    transition()
+    
 def ans_no():
     data[win.cur_num, 1] = 0
-    next()
-    print(data)
+    transition()
 
 btn_no.clicked.connect(ans_no)
 btn_yes.clicked.connect(ans_yes)
 
-next()
+next_q()
 
 
 win.show()
